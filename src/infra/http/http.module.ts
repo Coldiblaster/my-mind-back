acb8c1f63889160a18dbb7676e8806896dfa7ea7
coldiblaster <vbastazin@gmail.com>
2024-11-04 15:07:53 -0300

    refactor: alterando nomenclatura

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -5,11 +5,11 @@ import { RegisterCompanyUseCase } from '@/domain/register/application/use-cases/
 import { CryptographyModule } from '../cryptography/cryptography.module';
 import { DatabaseModule } from '../database/database.module';
 import { StorageModule } from '../storage/storage.module';
-import { CreateCompanyController } from './controller/create-company.controller';
+import { RegisterController } from './controller/register.controller';
 
 @Module({
   imports: [DatabaseModule, CryptographyModule, StorageModule],
-  controllers: [CreateCompanyController],
+  controllers: [RegisterController],
   providers: [RegisterCompanyUseCase],
 })
 export class HttpModule { }
