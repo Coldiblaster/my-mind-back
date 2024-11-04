f0c3a5eac96026aacc6a1584003c480b522b920b
coldiblaster <vbastazin@gmail.com>
2024-11-04 15:06:38 -0300

    refactor: alterando nomenclatura

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -6,10 +6,10 @@ import { EnvService } from './env/env.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const envService = app.get(EnvService);
-  const port = envService.get('PORT');
+  const configService = app.get(EnvService);
+  const port = configService.get('PORT');
 
-  await app.listen(3333);
+  await app.listen(port);
 }
 
 bootstrap();
