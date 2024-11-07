fd5b69becf5ce1df1a749d7b4394b706eb933ce4
coldiblaster <vbastazin@gmail.com>
2024-11-07 15:48:56 -0300

    feat: melhorando validação do token e adicionando retorno de dados para os testes

--- src/infra/auth/clerk.strategy.ts
+++ src/infra/auth/clerk.strategy.ts
@@ -1,4 +1,4 @@
-import { getAuth, verifyToken } from '@clerk/express';
+import { verifyToken } from '@clerk/express';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
@@ -8,7 +8,7 @@ import { z } from 'zod';
 import { EnvService } from '../env/env.service';
 
 const tokenPayloadSchema = z.object({
-  professionalId: z.string().uuid(),
+  providerId: z.string().uuid(),
 });
 
 export type UserPayload = z.infer<typeof tokenPayloadSchema>;
@@ -26,24 +26,28 @@ export class ClerkStrategy extends PassportStrategy(Strategy, 'clerk') {
       throw new UnauthorizedException('Token de autenticação ausente');
     }
 
+    if (process.env.NODE_ENV === 'test') {
+      return {
+        providerId: 'mocked-provider-id',
+      };
+    }
+
     try {
       const token = authHeader.replace('Bearer ', '');
 
-      await verifyToken(token, {
+      const auth = await verifyToken(token, {
         secretKey: this.envService.get('CLERK_SECRET_KEY'),
       });
+
+      if (!auth.professionalId) {
+        throw new UnauthorizedException('Sessão inválida ou não encontrada');
+      }
+
+      return {
+        providerId: auth.professionalId,
+      };
     } catch {
-      throw new UnauthorizedException('Sessão inválida ou não encontrada');
-    }
-
-    const auth = getAuth(req);
-
-    if (!auth.sessionClaims || !auth.sessionClaims.professionalId) {
       throw new UnauthorizedException('Token inválido');
     }
-
-    return {
-      professionalId: auth.sessionClaims.professionalId,
-    };
   }
 }
