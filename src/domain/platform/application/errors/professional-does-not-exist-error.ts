e1b7ab0f9beb90a6141dee3d30d3b6e534d65c5d
coldiblaster <vbastazin@gmail.com>
2024-11-07 15:47:39 -0300

    feat: add message error

---
+++ src/domain/platform/application/errors/professional-does-not-exist-error.ts
@@ -0,0 +1,9 @@
+import { UseCaseError } from '@/core/errors/use-case-error';
+
+export class ProfessionalDoesNotExistError
+  extends Error
+  implements UseCaseError {
+  constructor(identifier: string) {
+    super(`Professional "${identifier}" does not exist.`);
+  }
+}
