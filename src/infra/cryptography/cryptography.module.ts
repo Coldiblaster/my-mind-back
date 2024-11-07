90fa0ac5497730ede129855b3e8cc7754d4ebd60
coldiblaster <vbastazin@gmail.com>
2024-11-07 15:47:29 -0300

    feat: add jwt encrypter para criar tokens para testes e2e

---
+++ src/infra/cryptography/cryptography.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+
+import { Encrypter } from '@/domain/platform/application/cryptography/encrypter';
+
+import { JwtEncrypter } from './jwt-encrypter';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'default_secret_key', // defina uma chave secreta para JWT
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [
+    {
+      provide: Encrypter,
+      useClass: JwtEncrypter,
+    },
+  ],
+  exports: [Encrypter],
+})
+export class CryptographyModule { }
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,13 +3,14 @@ import { Module } from '@nestjs/common';
 import { CreateServiceUseCase } from '@/domain/platform/application/use-cases/service/create-service';
 import { RegisterCompanyUseCase } from '@/domain/register/application/use-cases/register-company';
 
+import { CryptographyModule } from '../cryptography/cryptography.module';
 import { DatabaseModule } from '../database/database.module';
 import { StorageModule } from '../storage/storage.module';
 import { RegisterController } from './controller/register.controller';
 import { CreateServiceController } from './controller/service/create-service.controller';
 
 @Module({
-  imports: [DatabaseModule, StorageModule],
+  imports: [DatabaseModule, StorageModule, CryptographyModule],
   controllers: [RegisterController, CreateServiceController],
   providers: [RegisterCompanyUseCase, CreateServiceUseCase],
 })
