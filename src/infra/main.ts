f72b6e60248f1fc58002e8ba4d6762c7e430f1bf
coldiblaster <vbastazin@gmail.com>
2024-11-08 11:00:32 -0300

    feat: inicializando swagger

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { patchNestJsSwagger } from 'nestjs-zod';
 
 import { AppModule } from './app.module';
 import { EnvService } from './env/env.service';
@@ -8,6 +10,27 @@ async function bootstrap() {
 
   const configService = app.get(EnvService);
   const port = configService.get('PORT');
+
+  patchNestJsSwagger();
+
+  const config = new DocumentBuilder()
+    .setTitle('My.Mind')
+    .setDescription('The description of the my mind API')
+    .setVersion('1.0')
+    .addTag('Mind')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+    })
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory, {
+    swaggerOptions: {
+      persistAuthorization: true, // Mantém a autorização entre atualizações de página
+    },
+  });
 
   await app.listen(port);
 }
