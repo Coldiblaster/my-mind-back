6bc9dcef5854f0d214e12480f512a0edf92181c2
coldiblaster <vbastazin@gmail.com>
2024-11-08 11:02:27 -0300

    refactor: indentando codigo

--- src/domain/platform/application/errors/professional-does-not-exist-error.ts
+++ src/domain/platform/application/errors/professional-does-not-exist-error.ts
@@ -2,7 +2,8 @@ import { UseCaseError } from '@/core/errors/use-case-error';
 
 export class ProfessionalDoesNotExistError
   extends Error
-  implements UseCaseError {
+  implements UseCaseError
+{
   constructor(identifier: string) {
     super(`Professional "${identifier}" does not exist.`);
   }
