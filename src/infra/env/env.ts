e7c55d10abc4119befc30cf47dfb1d0b45617c68
coldiblaster <vbastazin@gmail.com>
2024-11-11 15:06:24 -0300

    feat: add key OPENAI_API_KEY

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -13,6 +13,7 @@ export const envSchema = z.object({
   REDIS_PASSWORD: z.coerce.string().optional(),
   REDIS_DB: z.coerce.number().optional().default(0),
   PORT: z.coerce.number().optional().default(3333),
+  OPENAI_API_KEY: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
