8e27598804a43357357b6c3eb4f7e15c5aa13b02
coldiblaster <vbastazin@gmail.com>
2024-11-11 15:06:33 -0300

    feat: add BusinessTypeModule

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -5,11 +5,18 @@ import { RegisterCompanyUseCase } from '@/domain/register/application/use-cases/
 import { CryptographyModule } from '../cryptography/cryptography.module';
 import { DatabaseModule } from '../database/database.module';
 import { StorageModule } from '../storage/storage.module';
+import { BusinessTypeModule } from './controller/business-type/business-type.module';
 import { RegisterController } from './controller/register.controller';
 import { ServiceModule } from './controller/service/service.module';
 
 @Module({
-  imports: [DatabaseModule, StorageModule, CryptographyModule, ServiceModule],
+  imports: [
+    DatabaseModule,
+    StorageModule,
+    CryptographyModule,
+    ServiceModule,
+    BusinessTypeModule,
+  ],
   controllers: [RegisterController],
   providers: [RegisterCompanyUseCase],
 })
