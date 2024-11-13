cd1321e7e601013329cea5edf3e942a29a7a60ad
coldiblaster <vbastazin@gmail.com>
2024-11-12 21:55:20 -0300

    refactor: ajustando estrtura

---
+++ prisma/migrations/20241113004613_add_mandatory_coluns_table_client/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - Made the column `phone` on table `clients` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `document` on table `clients` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "clients" ALTER COLUMN "phone" SET NOT NULL,
+ALTER COLUMN "document" SET NOT NULL;
